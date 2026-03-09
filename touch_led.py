d31686e66804a5f9d19ec02bd5de4b038223d96d
Prathiksha D V <prathikshadv2005@gmail.com>
2026-03-09 14:15:24 +0530

    Add touch sensor control for LED
    Implement touch sensor to toggle LED state.

---
+++ touch_led.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+TOUCH_PIN = 17
+LED_PIN = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TOUCH_PIN, GPIO.IN)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+led_state = False
+
+try:
+    while True:
+        if GPIO.input(TOUCH_PIN) == 1:
+            led_state = not led_state
+            GPIO.output(LED_PIN, led_state)
+            time.sleep(0.5)  # debounce delay
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
